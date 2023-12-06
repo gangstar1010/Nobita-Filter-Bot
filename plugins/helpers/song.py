760e700d9758557bc7fc05c122f76759556be10c
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2023-12-06 15:24:03 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [ᴄʜᴀɴɴᴇʟ](https://youtube.com/@MovieVillaYT)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [ᴍᴏᴠɪᴇ ᴠɪʟʟᴀ](https://telegram.me/MovieVillaYT)**'
+        rep = '**Uᴘᴅᴀᴛᴇꜱ Cʜᴀɴɴᴇʟ ›› [ᴄʜᴀɴɴᴇʟ](https://telegram.me/SH_OTT)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [SHPʀᴏSᴇᴀʀᴄʜʙᴏᴛ](https://telegram.me/SHProSearchbot)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
